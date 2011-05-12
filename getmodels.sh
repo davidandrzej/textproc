1904378b1631501dd3f050f53d06b9ee1a149105
David Andrzejewski <david.andrzej@gmail.com>
2011-05-12 14:38:09 -0700

    fix getmodels script

--- getmodels.sh
+++ getmodels.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-wget http://opennlp.sourceforge.net/models-1.5/en-token.bin
-wget http://opennlp.sourceforge.net/models-1.5/en-sent.bin
+MODELDIR=./src/main/resources/models/
+
+URLS=(
+    'http://opennlp.sourceforge.net/models-1.5/en-token.bin'
+    'http://opennlp.sourceforge.net/models-1.5/en-sent.bin'
+)
+
+for URL in ${URLS[@]}
+do
+    wget $URL --directory-prefix=$MODELDIR
+done
 
