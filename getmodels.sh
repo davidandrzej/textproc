fb1a90147d136d9d5f7c907f26fb6a5bd150786a
David Andrzejewski <david.andrzej@gmail.com>
2011-05-16 16:50:37 -0700

    download english.stop stoplist as well

--- getmodels.sh
+++ getmodels.sh
@@ -5,6 +5,7 @@ MODELDIR=./src/main/resources/models/
 URLS=(
     'http://opennlp.sourceforge.net/models-1.5/en-token.bin'
     'http://opennlp.sourceforge.net/models-1.5/en-sent.bin'
+    'http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop'
 )
 
 for URL in ${URLS[@]}
